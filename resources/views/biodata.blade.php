9bfac56347ceed3e16af0eaadca394af9597cb6a
azizulweb <muhanifazizulfikri@gmail.com>
2025-04-05 11:43:27 +0700

    first commit

--- resources/views/biodata.blade.php
+++ resources/views/biodata.blade.php
@@ -1,6 +1,6 @@
 <x-layout title="biodata">
     <div class="bg-white p-6 rounded-lg shadow-md">
-        <h2 class="text-lg font-semibold text-gray-700">Isi Biodata Anda!!!!</h2>
+        <h2 class="text-lg font-semibold text-gray-700">Isi Biodata Anda!</h2>
         <p class="text-gray-500 mt-2">Silakan mulai mengelola sistem informasi akademik Anda.</p>
     </div>
 </x-layout>
